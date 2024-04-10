a73b46056ae58b2f2a14bfd8e0e5248cfe0f8d4f
sangay wangchuk <sangaywangchuk100@gmail.com>
2024-04-10 22:58:21 +0600

    project setup

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+const buildPrettierCommand = () => "prettier . --write --ignore-unknown";
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+};
